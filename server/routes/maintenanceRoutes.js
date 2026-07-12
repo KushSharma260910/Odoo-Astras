e653fadac15beb2ea8428c23e133d5d4e0d9022d
MetaKushal <kushalrangoonwala9@gmail.com>
2026-07-12 16:10:58 +0530

    changes in apis

--- server/routes/maintenanceRoutes.js
+++ server/routes/maintenanceRoutes.js
@@ -1,25 +1,22 @@
+const express = require("express");
 
-const express=require("express");
+const router = express.Router();
 
-const router=express.Router();
+const maintenanceController = require("../controllers/maintenanceController");
 
-const maintenanceController=require("../controllers/maintenanceController");
-
-router.get("/",maintenanceController.getAllMaintenance);
-
-router.get("/:id",maintenanceController.getMaintenanceById);
-
-// router.post("/",maintenanceController.createMaintenance);
+router.get(
+    "/",
+    maintenanceController.getAllMaintenance
+);
 
 router.post(
     "/",
-    authMiddleware,
-    roleMiddleware("FLEET_MANAGER"),
     maintenanceController.createMaintenance
 );
 
-router.patch("/:id/complete",maintenanceController.completeMaintenance);
+router.patch(
+    "/:id/close",
+    maintenanceController.closeMaintenance
+);
 
-router.delete("/:id",maintenanceController.deleteMaintenance);
-
-module.exports=router;
+module.exports = router;
